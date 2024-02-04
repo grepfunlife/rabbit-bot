cbe2e084a1b43d8d00ed2abc524c699189b30849
Mia <smartundschon@gmail.com>
2024-02-04 11:32:06 +0100

    added registration command

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 val telegramVersion: String by project
+val ktorVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
 }
 
 group = "red.rabbit"
@@ -19,6 +21,13 @@ dependencies {
             strictly("1.16.0")
         }
     }
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
+    implementation("io.ktor:ktor-client-apache5:$ktorVersion")
+
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
+
+
 fun main() {
 
+    try {
+        val botsApi = TelegramBotsApi(DefaultBotSession::class.java)
+
+        botsApi.registerBot(RabbitBot())
+    } catch (e: TelegramApiException) {
+        e.printStackTrace()
+    }
 }
